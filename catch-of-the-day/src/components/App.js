d526dffc9cee394be21a18209d2bcbd2780d63f5
Brandt <bmjorgensen@gmail.com>
2020-11-29 21:04:08 -0600

    load sample data

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import Header from './Header'
-import Order from './Order'
-import Inventory from './Inventory'
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
   };
+
   addFish = (fish) => {
     // THREE STEP PROCESS
     // 1. Take a copy of the existing state, avoid mutations
@@ -21,14 +24,22 @@ class App extends React.Component {
     // this.setState({ fishes: fishes });
     // Just pass the piece of state you wish to update, not the entire thing
   };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            { Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
     );
   }
