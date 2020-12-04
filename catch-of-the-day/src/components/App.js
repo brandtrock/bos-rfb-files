e51e34bc8d176a98c56a08bce06af7c887fab95b
Brandt <bmjorgensen@gmail.com>
2020-12-03 21:38:20 -0600

    remove items functionality

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -56,6 +56,15 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  deleteFish = (key) => {
+    // 1. Take a copy of state
+    const fishes = { ...this.state.fishes };
+    // 2. Update state - remove item
+    fishes[key] = null;
+    // 3. Update state
+    this.setState({ fishes });
+  };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -66,6 +75,15 @@ class App extends React.Component {
     // 2. Either add to the order OR update the number in our order
     order[key] = order[key] + 1 || 1;
     // 3. Call setState to update our state object
+    this.setState({ order });
+  };
+
+  removeFromOrder = (key) => {
+    // 1. Take a copy of state
+    const order = { ...this.state.order };
+    // 2. Update state - remove item
+    delete order[key];
+    // 3. Update state
     this.setState({ order });
   };
 
@@ -80,10 +98,11 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder} />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
         />
